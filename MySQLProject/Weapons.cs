2597b667008d8a8d870eb6ede92e61235e187dbb
stbrown <shadowtuner1@gmail.com>
2018-10-05 19:17:32 -0500

    Initial Commit including setting up AppSettings files, Weapon and WeaponsRepository files, and Program file

---
+++ MySQLProject/Weapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySQLProject
+{
+    public class Weapons
+    {
+        public string WeaponName { get; set; }
+        public int WeaponType { get; set; }
+        public int Rarity { get; set; }
+        public string Slot { get; set; }
+        public int Attack { get; set; }
+        public int Impact { get; set; }
+        public int Magazine { get; set; }
+    }
+}
---
+++ MySQLProject/WeaponsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MySQLProject
+{
+    public class WeaponsRepository
+    {
+        public WeaponsRepository()
+        {
+        }
+    }
+}
